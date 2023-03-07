99dafb4b168b6739fb4b8a9e1e23591262001654
alisher <aytbayalisher@gmail.com>
2023-03-07 12:39:31 +0600

    switch cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,20 @@ import {PostComponent} from './post/post.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  objArray: Array<any> = [
-    {id:1,postTitle:'Post 1'},
-    {id:2,postTitle:'Post 2'},
-    {id:3,postTitle:'Post 3'},
-    {id:4,postTitle:'Post 4'},
-    {id:5,postTitle:'Post 5'},
+  stepForm:string;
 
-  ]
-  onClick(){
-    let len = this.objArray.length+1
-    this.objArray.push({id:len,postTitle:'Post '+len})
-  }
-  onDelete(post){
-    let id  = post.id
-    this.objArray = this.objArray.filter(item => item.id !== id);
-  }
-  postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5']
 
+  onClick(status){
+    this.stepForm = status;
+  }
   constructor() {
 
   }
 }
 
+
+// onDelete(post){
+//   let id  = post.id
+//   this.objArray = this.objArray.filter(item => item.id !== id);
+// }
+// postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5']
